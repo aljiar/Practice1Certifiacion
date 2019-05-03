92c6a7402f0370cf8036b1698038ce88bf15c28c
Alan Jimenez Arandia <alanjimenez@Alans-MacBook-Pro.local>
2019-05-02 20:02:13 -0400

    Modify project:using components

---
+++ src/components/ShowForm.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="product">
+
+    <div class="product-image">
+      <img v-bind:src="image">
+    </div>
+
+    <div class="product-info">
+      <h1>{{ title }}</h1>
+      <p v-if="inStock"> In Stock</p>
+      <p v-else> Out of Stock</p>
+
+      <!--<ul>
+        <li v-for="detail in details"> {{ detail }} </li>
+      </ul>-->
+
+      <div v-for="(variant, index) in variants"
+           :key="variant.variantId"
+           class="color-box"
+           :style="{ backgroundColor: variant.variantColor }"
+           @mouseover="updateProduct(index)">
+      </div>
+
+      <button v-on:click="addToCart" 
+              :disabled="!inStock"
+              :class="{disabledButton: !inStock}" >
+              Add to cart
+        </button>
+
+    </div>
+
+    <div>
+        <h2>Reviews</h2>
+        <p v-if="!reviews.length">There are no reviews yet.</p>
+        <!--<ul>
+            <li v-for="review in reviews">
+                <p>{{ review.name }}</p>
+                <p>Rating: {{ review.rating }}</p>
+                <p>{{ review.review }}</p>
+            </li>
+        </ul>-->
+    </div>
+
+    <product-review @review-submitted="addReview" ></product-review>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+        return {
+            brand: 'Apple',
+            product: 'iPhone',
+            selectedVariant: 0,
+            details: ["Capacity:64 GB","Model:Max","Finish:Glossy"],
+            variants:[
+                {
+                    variantId:2234,
+                    variantColor:"Gold",
+                    variantQuantity:10,
+                    variantImage:"iphonegold.jpg"
+                },
+                {
+                    variantId:2235,
+                    variantColor:"White",
+                    variantQuantity:20,
+                    variantImage:"iphonewhite.jpg"
+                },
+                {
+                    variantId:2236,
+                    variantColor:"Black",
+                    variantQuantity:0,
+                    variantImage:"iphoneblack.jpg"
+                }
+            ],
+            reviews: []
+        }
+    },
+    methods: {
+        addToCart: function(){
+            this.$emit('add-to-cart',this.variants[this.selectedVariant].variantId)
+        },
+        updateProduct: function(index){
+            this.selectedVariant = index
+            console.log(index)
+        },
+        addReview(productReview){
+            this.reviews.push(productReview)
+        }
+    },
+     computed: {
+         title(){
+             return this.brand + ' ' + this.product
+         },
+         image(){
+            return this.variants[this.selectedVariant].variantImage
+         },
+         inStock(){
+             return this.variants[this.selectedVariant].variantQuantity
+         }
+     }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ShowForm msg="Welcome to Your Vue.js App"/>
+    <ReviewForm/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import ShowForm from '@/components/ShowForm.vue'
+import ReviewForm from '@/components/ReviewForm.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    ShowForm,
+    ReviewForm
   }
 }
 </script>
